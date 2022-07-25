614a6bde7c14caf790b01f2f7dfe807c0c1d42c3
Steve Clarke <84364906+s7clarke10@users.noreply.github.com>
2022-07-25 06:57:33 +0000

    Initial build - support for CDC / Log based

--- tap_mssql/__init__.py
+++ tap_mssql/__init__.py
@@ -25,6 +25,7 @@ from singer.catalog import Catalog, CatalogEntry
 import tap_mssql.sync_strategies.common as common
 import tap_mssql.sync_strategies.full_table as full_table
 import tap_mssql.sync_strategies.incremental as incremental
+import tap_mssql.sync_strategies.log_based as log_based
 
 from tap_mssql.connection import connect_with_backoff, MSSQLConnection
 
@@ -326,6 +327,35 @@ def desired_columns(selected, table_schema):
 
 
 def is_valid_currently_syncing_stream(selected_stream, state):
+    stream_metadata = metadata.to_map(selected_stream.metadata)
+    replication_method = stream_metadata.get((), {}).get('replication-method')
+
+    if replication_method != 'LOG_BASED':
+        return True
+
+    if replication_method == 'LOG_BASED' and cdc_stream_requires_historical(selected_stream, state):
+        return True
+
+    return False
+
+
+def cdc_stream_requires_historical(catalog_entry, state):
+
+    current_lsn = singer.get_bookmark(state,
+                                  catalog_entry.tap_stream_id,
+                                  'lsn')
+
+    max_lsn_values = singer.get_bookmark(state,
+                                        catalog_entry.tap_stream_id,
+                                        'max_lsn_values')
+
+    last_lsn_fetched = singer.get_bookmark(state,
+                                          catalog_entry.tap_stream_id,
+                                          'last_lsn_fetched')
+
+    if (current_lsn) and (not max_lsn_values and not last_lsn_fetched):
+        return False
+
     return True
 
 
@@ -373,7 +403,7 @@ def resolve_catalog(discovered_catalog, streams_to_sync):
     return result
 
 
-def get_non_binlog_streams(mssql_conn, catalog, config, state):
+def get_non_cdc_streams(mssql_conn, catalog, config, state):
     """Returns the Catalog of data we're going to sync for all SELECT-based
     streams (i.e. INCREMENTAL, FULL_TABLE, and LOG_BASED that require a historical
     sync). LOG_BASED streams that require a historical sync are inferred from lack
@@ -411,8 +441,20 @@ def get_non_binlog_streams(mssql_conn, catalog, config, state):
         stream_state = state.get("bookmarks", {}).get(stream.tap_stream_id)
 
         if not stream_state:
+            if replication_method == 'LOG_BASED':
+                LOGGER.info("LOG_BASED stream %s requires full historical sync", stream.tap_stream_id)
+
             streams_without_state.append(stream)
-        else:
+        elif stream_state and replication_method == 'LOG_BASED' and cdc_stream_requires_historical(stream, state):
+            is_view = common.get_is_view(stream)
+
+            if is_view:
+                raise Exception("Unable to replicate stream({}) with cdc because it is a view.".format(stream.stream))
+
+            LOGGER.info("LOG_BASED stream %s will resume its historical sync", stream.tap_stream_id)
+
+            streams_with_state.append(stream)
+        elif stream_state and replication_method != 'LOG_BASED':
             streams_with_state.append(stream)
 
     # If the state says we were in the middle of processing a stream, skip
@@ -441,18 +483,21 @@ def get_non_binlog_streams(mssql_conn, catalog, config, state):
     return resolve_catalog(discovered, streams_to_sync)
 
 
-def get_binlog_streams(mssql_conn, catalog, config, state):
+def get_cdc_streams(mssql_conn, catalog, config, state):
     discovered = discover_catalog(mssql_conn, config)
 
     selected_streams = list(filter(lambda s: common.stream_is_selected(s), catalog.streams))
-    binlog_streams = []
+    cdc_streams = []
 
     for stream in selected_streams:
         stream_metadata = metadata.to_map(stream.metadata)
         replication_method = stream_metadata.get((), {}).get("replication-method")
         stream_state = state.get("bookmarks", {}).get(stream.tap_stream_id)
 
-    return resolve_catalog(discovered, binlog_streams)
+        if replication_method == 'LOG_BASED' and not cdc_stream_requires_historical(stream, state):
+            cdc_streams.append(stream)
+
+    return resolve_catalog(discovered, cdc_streams)
 
 
 def write_schema_message(catalog_entry, bookmark_properties=[]):
@@ -480,6 +525,30 @@ def do_sync_incremental(mssql_conn, config, catalog_entry, state, columns):
     singer.write_message(singer.StateMessage(value=copy.deepcopy(state)))
 
 
+def do_sync_historical_log(mssql_conn, config, catalog_entry, state, columns):
+    key_properties = common.get_key_properties(catalog_entry)
+    mssql_conn = MSSQLConnection(config)
+
+    # Add additional keys to the schema
+    log_based.add_synthetic_keys_to_schema(catalog_entry)
+
+    write_schema_message(catalog_entry)
+
+    stream_version = common.get_stream_version(catalog_entry.tap_stream_id, state)
+
+    # full_table.sync_table(mssql_conn, config, catalog_entry, state, columns, stream_version)
+    log_based.sync_historic_table(mssql_conn, config, catalog_entry, state, columns, stream_version)    
+
+    # Prefer initial_full_table_complete going forward
+    singer.clear_bookmark(state, catalog_entry.tap_stream_id, "version")
+
+    state = singer.write_bookmark(
+        state, catalog_entry.tap_stream_id, "initial_full_table_complete", True
+    )
+
+    singer.write_message(singer.StateMessage(value=copy.deepcopy(state)))
+
+
 def do_sync_full_table(mssql_conn, config, catalog_entry, state, columns):
     key_properties = common.get_key_properties(catalog_entry)
     mssql_conn = MSSQLConnection(config)
@@ -497,11 +566,26 @@ def do_sync_full_table(mssql_conn, config, catalog_entry, state, columns):
 
     singer.write_message(singer.StateMessage(value=copy.deepcopy(state)))
 
+def do_sync_log_based(mssql_conn, config, catalog_entry, state, columns):
+    mssql_conn = MSSQLConnection(config)
+    md_map = metadata.to_map(catalog_entry.metadata)
+    stream_version = common.get_stream_version(catalog_entry.tap_stream_id, state)
+    replication_key = md_map.get((), {}).get("replication-key")
+    # Add additional keys to the schema
+    log_based.add_synthetic_keys_to_schema(catalog_entry)
 
-def sync_non_binlog_streams(mssql_conn, non_binlog_catalog, config, state):
+    write_schema_message(catalog_entry=catalog_entry, bookmark_properties=[replication_key])
+    LOGGER.info("Schema written")
+    stream_version = common.get_stream_version(catalog_entry.tap_stream_id, state)
+    log_based.sync_table(mssql_conn, config, catalog_entry, state, columns, stream_version)
+
+    singer.write_message(singer.StateMessage(value=copy.deepcopy(state)))
+
+
+def sync_non_cdc_streams(mssql_conn, non_cdc_catalog, config, state):
     mssql_conn = MSSQLConnection(config)
 
-    for catalog_entry in non_binlog_catalog.streams:
+    for catalog_entry in non_cdc_catalog.streams:
         columns = list(catalog_entry.schema.properties.keys())
 
         if not columns:
@@ -517,9 +601,10 @@ def sync_non_binlog_streams(mssql_conn, non_binlog_catalog, config, state):
         replication_method = md_map.get((), {}).get("replication-method")
         replication_key = md_map.get((), {}).get("replication-key")
         primary_keys = common.get_key_properties(catalog_entry)
+        start_lsn = md_map.get((), {}).get("lsn")
         LOGGER.info(f"Table {catalog_entry.table} proposes {replication_method} sync")
         if not replication_method and config.get("default_replication_method"):
-            replication_method= config.get("default_replication_method") 
+            replication_method= config.get("default_replication_method")
             LOGGER.info(f"Table {catalog_entry.table} reverting to DEFAULT {replication_method} sync")
 
         if replication_method == "INCREMENTAL" and not replication_key:
@@ -528,7 +613,12 @@ def sync_non_binlog_streams(mssql_conn, non_binlog_catalog, config, state):
         if replication_method == "INCREMENTAL" and not primary_keys:
             LOGGER.info(f"No primary key for {catalog_entry.table}, using full table replication")
             replication_method = "FULL_TABLE"
-        LOGGER.info(f"Table {catalog_entry.table} will use {replication_method} sync")
+        if replication_method == "LOG_BASED" and not start_lsn:
+            LOGGER.info(
+                f"No initial load for {catalog_entry.table}, using full table replication"
+            )
+        else:        
+            LOGGER.info(f"Table {catalog_entry.table} will use {replication_method} sync")
 
         database_name = common.get_database_name(catalog_entry)
 
@@ -542,19 +632,65 @@ def sync_non_binlog_streams(mssql_conn, non_binlog_catalog, config, state):
             elif replication_method == "FULL_TABLE":
                 LOGGER.info(f"syncing {catalog_entry.table} full table")
                 do_sync_full_table(mssql_conn, config, catalog_entry, state, columns)
+            elif replication_method == "LOG_BASED":
+                LOGGER.info(f"syncing {catalog_entry.table} cdc tables")
+                do_sync_historical_log(mssql_conn, config, catalog_entry, state, columns)               
             else:
-                raise Exception("only INCREMENTAL and FULL TABLE replication methods are supported")
+                raise Exception("only INCREMENTAL, LOG_BASED and FULL TABLE replication methods are supported")
 
     state = singer.set_currently_syncing(state, None)
     singer.write_message(singer.StateMessage(value=copy.deepcopy(state)))
 
 
+def sync_cdc_streams(mssql_conn, cdc_catalog, config, state):
+    mssql_conn = MSSQLConnection(config)
+
+    if cdc_catalog.streams:
+        for catalog_entry in cdc_catalog.streams:
+            columns = list(catalog_entry.schema.properties.keys())
+            if not columns:
+                LOGGER.warning(
+                    "There are no columns selected for stream %s, skipping it.", catalog_entry.stream
+                )
+                continue
+
+            state = singer.set_currently_syncing(state, catalog_entry.tap_stream_id)
+
+            # Emit a state message to indicate that we've started this stream
+            singer.write_message(singer.StateMessage(value=copy.deepcopy(state)))
+
+            md_map = metadata.to_map(catalog_entry.metadata)
+            replication_method = md_map.get((), {}).get("replication-method")
+            replication_key = md_map.get((), {}).get("replication-key")
+            primary_keys = md_map.get((), {}).get("table-key-properties")
+            LOGGER.info(f"Table {catalog_entry.table} proposes {replication_method} sync")
+            LOGGER.info(f"Table {catalog_entry.table} will use {replication_method} sync")
+
+            database_name = common.get_database_name(catalog_entry)
+
+            with metrics.job_timer('table_cdc_sync') as timer:
+                timer.tags["database"] = database_name
+                timer.tags["table"] = catalog_entry.table
+
+                if replication_method == "LOG_BASED":
+                    LOGGER.info(f"syncing {catalog_entry.table} cdc tables")
+                    do_sync_log_based(mssql_conn, config, catalog_entry, state, columns)                
+                else:
+                    raise Exception("only LOG_BASED methods are supported for CDC")
+
+        state = singer.set_currently_syncing(state, None)
+        singer.write_message(singer.StateMessage(value=copy.deepcopy(state)))                    
+
+
 def do_sync(mssql_conn, config, catalog, state):
     LOGGER.info("Beginning sync")
-    non_binlog_catalog = get_non_binlog_streams(mssql_conn, catalog, config, state)
-    for entry in non_binlog_catalog.streams:
+    non_cdc_catalog = get_non_cdc_streams(mssql_conn, catalog, config, state)
+    cdc_catalog = get_cdc_streams(mssql_conn, catalog, config, state)
+
+    for entry in non_cdc_catalog.streams:
         LOGGER.info(f"Need to sync {entry.table}")
-    sync_non_binlog_streams(mssql_conn, non_binlog_catalog, config, state)
+    sync_non_cdc_streams(mssql_conn, non_cdc_catalog, config, state)
+    sync_cdc_streams(mssql_conn, cdc_catalog, config, state)
 
 
 def log_server_params(mssql_conn):
---
+++ tap_mssql/sync_strategies/log_based.py
@@ -0,0 +1,283 @@
+#!/usr/bin/env python3
+# pylint: disable=duplicate-code,too-many-locals,simplifiable-if-expression
+
+import copy
+import singer
+from singer import metadata
+from singer.schema import Schema
+
+import tap_mssql.sync_strategies.common as common
+
+from tap_mssql.connection import connect_with_backoff, MSSQLConnection
+
+LOGGER = singer.get_logger()
+
+def py_bin_to_mssql(binary_value):
+    return "CONVERT(BINARY(10),'0x" + binary_value + "',1)"
+
+def verify_change_data_capture_table(connection, schema_name, table_name):
+    cur = connection.cursor()
+    cur.execute("""select s.name as schema_name, t.name as table_name, t.is_tracked_by_cdc, t.object_id
+                   from sys.tables t
+                   join sys.schemas s on (s.schema_id = t.schema_id)
+                   and  t.name = '{}'
+                   and  s.name = '{}'""".format(table_name,schema_name)
+               )
+    row = cur.fetchone()
+        
+    return row[2]
+
+def verify_change_data_capture_databases(connection):
+    cur = connection.cursor()
+    cur.execute("""SELECT name, is_cdc_enabled
+                   FROM sys.databases WHERE database_id = DB_ID()"""
+               )
+    row = cur.fetchone()
+
+    LOGGER.info(
+        "CDC Databases enable : Database %s, Enabled %s", *row,
+    )
+    return row
+
+def verify_read_isolation_databases(connection):
+    cur = connection.cursor()
+    cur.execute("""SELECT DB_NAME(database_id),
+                          is_read_committed_snapshot_on,
+                          snapshot_isolation_state_desc
+                   FROM sys.databases
+                   WHERE database_id = DB_ID();"""
+               )
+    row = cur.fetchone()
+
+    if row[1] == False and row[2] == "OFF":
+        LOGGER.warning(
+            "CDC Databases may result in dirty reads. Consider enabling Read Committed or Snapshot isolation: Database %s, Is Read Committed Snapshot is %s, Snapshot Isolation is %s", *row,
+        )
+    return row    
+
+def get_lsn_available_range(connection, capture_instance_name ):
+    cur = connection.cursor()
+    query = """SELECT sys.fn_cdc_get_min_lsn ( '{}' ) lsn_from
+                    , sys.fn_cdc_get_max_lsn () lsn_to
+               ;
+            """.format(capture_instance_name)
+    cur.execute(query)
+    row = cur.fetchone()
+
+    if row[0] is None:   # Test that the lsn_from is not NULL i.e. there is change data to process
+       LOGGER.info("No data available to process in CDC table %s", capture_instance_name)
+    else:
+       LOGGER.info("Data available in cdc table %s from lsn %s", capture_instance_name, row[0].hex())
+
+    return row
+
+def get_to_lsn(connection):
+    cur = connection.cursor()
+    query = """select sys.fn_cdc_get_max_lsn () """
+
+    cur.execute(query)
+    row = cur.fetchone()
+
+    LOGGER.info(
+        "Max LSN ID : %s", row[0].hex(),
+    )
+    return row 
+
+def add_synthetic_keys_to_schema(catalog_entry):
+   catalog_entry.schema.properties['_sdc_operation_type'] = Schema(
+            description='Source operation I=Insert, D=Delete, U=Update', type=['null', 'string'], format='string')  
+   catalog_entry.schema.properties['_sdc_lsn_commit_timestamp'] = Schema(
+            description='Source system commit timestamp', type=['null', 'string'], format='date-time')
+   catalog_entry.schema.properties['_sdc_lsn_deleted_at'] = Schema(
+            description='Source system delete timestamp', type=['null', 'string'], format='date-time')
+   catalog_entry.schema.properties['_sdc_lsn_value'] = Schema(
+            description='Source system log sequence number (LSN)', type=['null', 'string'], format='string')
+   catalog_entry.schema.properties['_sdc_lsn_seq_value'] = Schema(
+            description='Source sequence number within the system log sequence number (LSN)', type=['null', 'string'], format='string')                          
+   catalog_entry.schema.properties['_sdc_lsn_operation'] = Schema(
+            description='The operation that took place (1=Delete, 2=Insert, 3=Update (Before Image), 4=Update (After Image) )', type=['null', 'integer'], format='integer')  
+
+   return catalog_entry          
+
+def generate_bookmark_keys(catalog_entry):
+    md_map = metadata.to_map(catalog_entry.metadata)
+    stream_metadata = md_map.get((), {})
+    replication_method = stream_metadata.get("replication-method")
+
+    ### TO_DO: 
+    ### 1. check the use of the top three values above and the parameter value, seem to not be required.
+    ### 2. check the base_bookmark_keys required
+    base_bookmark_keys = {
+        "last_lsn_fetched",
+        "max_lsn_values",
+        "lsn",
+        "version",
+        "initial_full_table_complete",
+    }
+
+    bookmark_keys = base_bookmark_keys
+
+    return bookmark_keys
+
+def sync_historic_table(mssql_conn, config, catalog_entry, state, columns, stream_version):
+    mssql_conn = MSSQLConnection(config)
+    common.whitelist_bookmark_keys(
+        generate_bookmark_keys(catalog_entry), catalog_entry.tap_stream_id, state
+    )
+
+    # Add additional keys to the columns
+    extended_columns = columns + ['_sdc_operation_type', '_sdc_lsn_commit_timestamp', '_sdc_lsn_deleted_at', '_sdc_lsn_value', '_sdc_lsn_seq_value', '_sdc_lsn_operation']    
+
+    bookmark = state.get("bookmarks", {}).get(catalog_entry.tap_stream_id, {})
+    version_exists = True if "version" in bookmark else False
+
+    initial_full_table_complete = singer.get_bookmark(
+        state, catalog_entry.tap_stream_id, "initial_full_table_complete"
+    )
+
+    state_version = singer.get_bookmark(state, catalog_entry.tap_stream_id, "version")
+
+    activate_version_message = singer.ActivateVersionMessage(
+        stream=catalog_entry.stream, version=stream_version
+    )
+
+    # For the initial replication, emit an ACTIVATE_VERSION message
+    # at the beginning so the records show up right away.
+    if not initial_full_table_complete and not (version_exists and state_version is None):
+        singer.write_message(activate_version_message)
+
+    with connect_with_backoff(mssql_conn) as open_conn:
+        with open_conn.cursor() as cur:
+
+            escaped_columns = [common.escape(c) for c in columns]
+            table_name      = catalog_entry.table
+            schema_name     = common.get_database_name(catalog_entry)
+
+            if not verify_change_data_capture_table(mssql_conn,schema_name,table_name):
+               raise Exception("Error {}.{}: does not have change data capture enabled. Call EXEC sys.sp_cdc_enable_table with relevant parameters to enable CDC.".format(schema_name,table_name))            
+
+            verify_read_isolation_databases(mssql_conn)
+
+            # Store the current database lsn number, will use this to store at the end of the initial load.
+            # Note: Recommend no transactions loaded when the initial loads are performed.
+            lsn_to = str(get_to_lsn(mssql_conn)[0].hex())
+
+            select_sql = """
+                            SELECT {}
+                                ,'I' _sdc_operation_type
+                                , cast('1900-01-01' as datetime) _sdc_lsn_commit_timestamp
+                                , null _sdc_lsn_deleted_at
+                                , '00000000000000000000' _sdc_lsn_value
+                                , '00000000000000000000' _sdc_lsn_seq_value
+                                , 1 as _sdc_lsn_operation
+                            FROM {}.{}
+                            ;""".format(",".join(escaped_columns), schema_name, table_name)          
+            params = {}
+
+            common.sync_query(
+                cur, catalog_entry, state, select_sql, extended_columns, stream_version, params, config
+            )
+            state = singer.write_bookmark(state, catalog_entry.tap_stream_id, 'lsn', lsn_to)
+
+    # store the state of the table lsn's after the initial load ready for the next CDC run
+    singer.write_message(singer.StateMessage(value=copy.deepcopy(state)))
+
+    # clear max pk value and last pk fetched upon successful sync
+    singer.clear_bookmark(state, catalog_entry.tap_stream_id, "max_pk_values")
+    singer.clear_bookmark(state, catalog_entry.tap_stream_id, "last_pk_fetched")
+
+    singer.write_message(activate_version_message)
+
+def sync_table(mssql_conn, config, catalog_entry, state, columns, stream_version):
+    mssql_conn = MSSQLConnection(config)
+    common.whitelist_bookmark_keys(
+        generate_bookmark_keys(catalog_entry), catalog_entry.tap_stream_id, state
+    )
+
+    # Add additional keys to the columns
+    extended_columns = columns + ['_sdc_operation_type', '_sdc_lsn_commit_timestamp', '_sdc_lsn_deleted_at', '_sdc_lsn_value', '_sdc_lsn_seq_value', '_sdc_lsn_operation']
+
+    bookmark = state.get("bookmarks", {}).get(catalog_entry.tap_stream_id, {})
+    version_exists = True if "version" in bookmark else False
+
+    initial_full_table_complete = singer.get_bookmark(
+        state, catalog_entry.tap_stream_id, "initial_full_table_complete"
+    )
+
+    state_version = singer.get_bookmark(state, catalog_entry.tap_stream_id, "version")
+
+    activate_version_message = singer.ActivateVersionMessage(
+        stream=catalog_entry.stream, version=stream_version
+    )
+
+    # For the initial replication, emit an ACTIVATE_VERSION message
+    # at the beginning so the records show up right away.
+    if not initial_full_table_complete and not (version_exists and state_version is None):
+        singer.write_message(activate_version_message)
+
+    with connect_with_backoff(mssql_conn) as open_conn:
+        with open_conn.cursor() as cur:
+
+            # start_lsn = min([singer.get_bookmark(state, s.tap_stream_id, 'lsn') for s in catalog_entry.stream])
+            state_last_lsn = singer.get_bookmark(state, catalog_entry.tap_stream_id, "lsn")
+            
+            escaped_columns = [common.escape(c) for c in columns]
+            table_name      = catalog_entry.table
+            schema_name     = common.get_database_name(catalog_entry)
+            schema_table   = schema_name + "_" + table_name
+
+            if not verify_change_data_capture_table(mssql_conn,schema_name,table_name):
+               raise Exception("Error {}.{}: does not have change data capture enabled. Call EXEC sys.sp_cdc_enable_table with relevant parameters to enable CDC.".format(schema_name,table_name))
+
+            # lsn_range = get_lsn_extract_range(mssql_conn, schema_name, table_name,"2016-08-01T23:00:00")
+            lsn_range = get_lsn_available_range(mssql_conn,schema_table)
+
+            if lsn_range[0] is not None:   # Test to see if there are any change records to process
+               lsn_from = str(lsn_range[0].hex())
+               lsn_to   = str(lsn_range[1].hex())
+            # if lsn_range[2] is not None:   # Test to see if there are any change records to process
+            #    lsn_from = str(lsn_range[2].hex())
+            #    lsn_to   = str(lsn_range[3].hex())
+
+               if lsn_from <= state_last_lsn:
+                  LOGGER.info("The last lsn processed as per the state file %s, minimum available lsn for extract table %s, and the maximum lsn is %s.", state_last_lsn, lsn_from, lsn_to)
+               else:
+                  raise Exception("Error {}.{}: CDC changes have expired, the minimum lsn is {}, the last processed lsn is {}. Recommend a full load as there may be missing data.".format(schema_name, table_name, lsn_from, state_last_lsn ))                
+
+               select_sql = """DECLARE @from_lsn binary (10), @to_lsn binary (10)
+                    
+                               SET @from_lsn = sys.fn_cdc_increment_lsn({})
+                               SET @to_lsn = {}
+
+                               SELECT {}
+                                   ,case __$operation
+                                       when 2 then 'I'
+                                       when 4 then 'U'
+                                       when 1 then 'D'
+                                   end _sdc_operation_type
+                                   , sys.fn_cdc_map_lsn_to_time(__$start_lsn) _sdc_lsn_commit_timestamp
+                                   , case __$operation
+                                       when 1 then sys.fn_cdc_map_lsn_to_time(__$start_lsn)
+                                       else null
+                                       end _sdc_lsn_deleted_at
+                                   , __$start_lsn _sdc_lsn_value
+                                   , __$seqval _sdc_lsn_seq_value 
+                                   , __$operation _sdc_lsn_operation
+                               FROM cdc.fn_cdc_get_all_changes_{}(@from_lsn, @to_lsn, 'all')
+                               ORDER BY __$start_lsn, __$seqval, __$operation
+                               ;""".format(py_bin_to_mssql(state_last_lsn), py_bin_to_mssql(lsn_to), ",".join(escaped_columns), schema_table )
+
+               params = {}
+
+               common.sync_query(
+                   cur, catalog_entry, state, select_sql, extended_columns, stream_version, params
+               )
+            
+               state = singer.write_bookmark(state, catalog_entry.tap_stream_id, 'lsn', lsn_to)
+               singer.write_message(singer.StateMessage(value=copy.deepcopy(state)))
+
+    # clear max lsn value and last lsn fetched upon successful sync
+    singer.clear_bookmark(state, catalog_entry.tap_stream_id, "max_lsn_values")
+    singer.clear_bookmark(state, catalog_entry.tap_stream_id, "last_lsn_fetched")
+
+    singer.write_message(activate_version_message)
